17368e9cacfea0cc158002396a5ff429d13fbe83
joaogularte <jov96@hotmail.com>
2018-11-25 19:03:33 -0200

    Adicionado algumas funcoes

--- getters.php
+++ getters.php
@@ -12,10 +12,39 @@
     }
 
     function getIP(){
-        echo shell_exec("hostname -I | cut -d ' ' -f1");
+        $ip = shell_exec("hostname -I | cut -d ' ' -f1");
+        echo "IP: ".$ip;
     }
+
+    function getMAC(){
+        $mac = shell_exec("ifconfig enp1s0 | grep ether | cut -d ' ' -f10");
+        echo "MAC: ".$mac;
+    }
+  
+    function getOS(){
+        echo "OS: ".PHP_OS."\n";
+    }
+
+    function getDev(){
+        echo "Nome do grupo: Joao, alcateia de um lobo só\n";
+    }
+
+    function getInfo(){
+        $info = shell_exec("uname -a");
+        echo "Infos do OS: ".$info;
+    }
+
+
+    function getDolar(){
+        
+    }
+    
     getWho();
     getTime();
     getIP();
-    
+    getMAC();
+    getOS();
+    getDev();
+    getInfo();
+
 ?>
