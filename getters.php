87a8e6aee583ed025802514120bfd472df86d533
Joao Gularte <joao.gularte@logycware.com>
2018-12-05 13:17:30 -0200

    concluido funcao getDolar

--- getters.php
+++ getters.php
@@ -44,25 +44,16 @@
 
         $in = "GET /USD/1 HTTP/1.1\r\n";
         $in .= "Host: economia.awesomeapi.com.br\r\n";
-        //$in .= "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0\r\n";
         $in .= "Connection: Close\r\n\r\n";
         $out = '';
 
         socket_write($socket, $in, strlen($in));
 
-        while($out = socket_read($socket, 2048)){
-            echo $out;
-        }
+        $out = socket_read($socket, 2048);
+        $pos = strpos($out, '[');
+        $json = substr($out, $pos);
+        $obj = json_decode($json);
+        echo $obj[0]->{'high'};
     }
-    echo getservbyname("https", "tcp");
-    echo gethostbyname("economia.awesomeapi.com.br");
-
-    getWho();
-    getTime();
-    getIP();
-    getMAC();
-    getOS();
-    getDev();
-    getInfo();
     getDolar();
 ?>
